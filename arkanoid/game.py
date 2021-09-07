90c186554b4ac11c7cae777711f7cb3d549147cb
Iberikos <Ruben89pm@gamil.com>
2021-09-07 19:15:14 +0200

    Initial commit

---
+++ arkanoid/game.py
@@ -0,0 +1,27 @@
+import pygame as pag
+
+from arkanoid import ALTO, ANCHO
+#from . import ALTO, ANCHO ---> se pueden de las 2 formas
+import sys
+
+
+
+pg.init()
+
+class Game():
+    def __init__(self):
+        self.pantalla = pg.display.set_mode((ANCHO, ALTO))
+        
+    def launch(self):
+        game_over = False
+        while not game_over:
+            for evento in pg.event.get():
+                if evento.type == pg.QUIT:
+                    game_over = True
+            
+            self.pantalla.fill((123, 123, 255))
+            
+            pg.display.flip()
+            
+        pg.quit()
+        sys.exit()
---
+++ main.py
@@ -0,0 +1,8 @@
+from arkanoid.game import Game
+
+if __name__ == '__main__':
+    game = Game ()
+    game.launch()
+    
+    #Game().launch() -----> es lo mismo que  game = Game () \ game.launch()
+    
