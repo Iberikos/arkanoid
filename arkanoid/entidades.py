68b98f3fecdd09913e1b2b8ee01c3205b47e4a6f
Iberikos <Ruben89pm@gamil.com>
2021-09-09 17:31:32 +0200

    Primera version jugable

--- arkanoid/entidades.py
+++ arkanoid/entidades.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.sprite import Sprite
+from . import ANCHO, ALTO
 
 class Raqueta(Sprite):
     disfraces = "electric00.png"
@@ -12,9 +13,44 @@ class Raqueta(Sprite):
         if pg.key.get_pressed()[pg.K_LEFT]:
             self.rect.x -=5
             
+        if self.rect.left <= 0:
+            self.rect.left = 0
+                        
         if pg.key.get_pressed()[pg.K_RIGHT]:
             self.rect.x +=5
+
+        if self.rect.right >= ANCHO:
+            self.rect.right = ANCHO
+                        
+class Bola(Sprite):
+    disfraces = "ball1.png"
+    def __init__(self, **kwargs):
+        self.image = pg.image.load(f"resources/images/{self.disfraces}")
+        self.rect = self.image.get_rect(**kwargs)
+        self.delta_x = 5
+        self.delta_y = 5
+        self.viva = True
+        self.posicion_inicial = kwargs
+        
+    def update(self):
+        self.rect.x += self.delta_x
+        if self.rect.x <=0 or self.rect.right >= ANCHO:
+            self.delta_x *= -1
             
+        self.rect.y += self.delta_y
+        if self.rect.y <=0:
+            self.delta_y *= -1    
+            
+        if self.rect.bottom >= ALTO:
+            self.viva = False
+            #self.rect.center = (ANCHO // 2, ALTO // 2)
+            self.rect = self.image.get_rect(**self.posicion_inicial)
+            
+    def comprobar_colision(self, otro):
+        if self.rect.right >= otro.rect.left and self.rect.left <= otro.rect.right and \
+           self.rect.bottom >= otro.rect.top and self.rect.top <= otro.rect.bottom:
+           self.delta_y *= -1
+
             
         
         
--- arkanoid/escenas.py
+++ arkanoid/escenas.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from . import FPS, ANCHO, ALTO
-from .entidades import Raqueta
+from .entidades import Raqueta, Bola
 
 class Escena():
     def __init__(self, pantalla):
@@ -44,19 +44,30 @@ class Partida(Escena):
         super().__init__(pantalla)
         self.fondo = pg.image.load("resources/images/background.jpg")
         self.player = Raqueta(midbottom = (ANCHO // 2, ALTO -15))
-
+        self.bola = Bola(center = (ANCHO // 2, ALTO // 2))
+        self.vidas = 3
+        
     def bucle_principal(self):
-        game_over = False
-        while not game_over:
+        vidas = 3
+        while vidas > 0:
             self.reloj.tick(FPS)
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
                     exit()
             
-            self.player.update()        
+            self.player.update()
+            self.bola.update()
+            self.bola.comprobar_colision(self.player)
+            
+            if not self.bola.viva:
+                vidas -= 1
+                self.bola.vida = True
+                
                     
             self.pantalla.blit(self.fondo, (0, 0))
             self.pantalla.blit(self.player.image, self.player.rect)
+            self.pantalla.blit(self.bola.image, self.bola.rect)
+            
             
             pg.display.flip()
 
